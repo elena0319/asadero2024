180223647b77f18c3e8eaa314eac354c517a51b9
elena20 <leup1991@gmail.com>
2024-08-20 12:47:57 -0500

    Mensaje de commit aquí

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::view('/','welcome');
-Route::resource("producto", App\Http\Controllers\ProductoController::class);
+Route::resource("productos", App\Http\Controllers\ProductoController::class);
 
 
 
