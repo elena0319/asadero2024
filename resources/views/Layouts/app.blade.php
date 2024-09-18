e05a8263d60fe00ae3067b0023f122276dd981a6
elena20 <leup1991@gmail.com>
2024-09-18 09:34:01 -0500

    Tu mensaje de commit

--- resources/views/Layouts/app.blade.php
+++ resources/views/Layouts/app.blade.php
@@ -1,29 +1,28 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>@yield ('titulo', 'asadero2024') </title>
-    @vite('resources/css/app.css')
-</head>
-<body>
-    <header>
-        {{-- Barra de navegación --}}
-        @include('layouts.navbar')
-    </header>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+       <meta charset="utf-8">
+       <meta name="viewport" content="width=device-width, initial-scale=1">
+       <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <main>
-        {{-- Contenido principal de cada página --}}
-        @yield('contenido')
-    </main>
+       <title>@yield('titulo','Minimercado2024')</title>
 
-    <footer>
-        {{-- Pie de página --}}
-        @include('layouts.footer')
-    </footer>
-</body>
+       <!-- Scripts -->
+       @vite(['resources/css/app.css', 'resources/js/app.js'])
+    </head>
+    <body>
+       <header>
+           {{-- Navbar --}}
+           @include('layouts.navbar')
+        </header>
+        <main>
+            <div class="bg-green-100 my-4 text-center">
+               <h1 class="text-lg font-semibold m-4 uppercase">@yield('cabecera')</h1>
+            </div>
+            @yield('contenido')
+        </main>
+        <footer class="footer items-center p-4 bg-neutral text-neutral-content">
+            @include('layouts.footer')
+        </footer>
+    </body>
 </html>
-
-    
-
